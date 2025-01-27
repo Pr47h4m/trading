51c7e116161e2398b80203b6aee5b0744d059bd7
Pratham Jaiswal <pr47h4m@gmail.com>
2025-01-27 14:01:08 +0530

    Added indicators/standard EMASeries

--- indicators/standard/ema.go
+++ indicators/standard/ema.go
@@ -1,9 +1,29 @@
 package standard
 
-import "github.com/pr47h4m/trading"
+import (
+	"fmt"
+	"github.com/pr47h4m/trading"
+)
 
 func EMASeries(candles []trading.Candler, period int) ([]trading.EMAValue, error) {
+	if len(candles) <= period {
+		return nil, fmt.Errorf("not enough data or invalid period")
+	}
 
+	ema := make([]trading.EMAValue, len(candles)-period+1)
+	multiplier := 2.0 / float64(period+1)
+
+	var smaSum float64
+	for i := 0; i < period; i++ {
+		smaSum += candles[i].GetClose()
+	}
+	initialEMA := smaSum / float64(period)
+	ema[0] = trading.EMAValue{Time: candles[period-1].GetTime(), Value: initialEMA}
+
+	for i := period; i < len(candles); i++ {
+		currentEMA := (candles[i].GetClose() * multiplier) + (ema[i-period].Value * (1 - multiplier))
+		ema[i-period+1] = trading.EMAValue{Time: candles[i].GetTime(), Value: currentEMA}
+	}
 	return nil, nil
 }
 
