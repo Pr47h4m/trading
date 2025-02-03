b7e641be92cc50bc1080fc2d0f9d82f5f6ec5443
Pratham Jaiswal <pr47h4m@gmail.com>
2025-02-03 21:49:10 +0530

    Improved types

--- types.go
+++ types.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Candle struct {
-	T  time.Time
-	O  float64 // The open price for the symbol in the given time period.
-	H  float64 // The highest price for the symbol in the given time period.
-	L  float64 // The lowest price for the symbol in the given time period.
-	C  float64 // The close price for the symbol in the given time period.
-	v  int     // The trading volume of the symbol in the given time period.
-	vw float64 // The volume weighted average price.
+	T  time.Time `json:"t" bson:"t"`
+	O  float64   `json:"o" bson:"o"`   // The open price for the symbol in the given time period.
+	H  float64   `json:"h" bson:"h"`   // The highest price for the symbol in the given time period.
+	L  float64   `json:"l" bson:"l"`   // The lowest price for the symbol in the given time period.
+	C  float64   `json:"c" bson:"c"`   // The close price for the symbol in the given time period.
+	V  int       `json:"v" bson:"v"`   // The trading volume of the symbol in the given time period.
+	VW float64   `json:"vw" bson:"vw"` // The volume weighted average price.
 }
 
 func (v Candle) String() string {
